3890c9fc9da133aafef4dc12b92699a292138f26
gggvgyver <gggvgyver@gmail.com>
2021-01-31 14:40:37 +0900

    49분

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
+import styles from '../styles/Header.module.css'
+
 export default () => (
-    <header>
-        별이야! ⓒ 2021 01 30
-    </header>
+    <div>
+        <div className={styles.title}>
+            <h1>
+                Nextjs와 strapi 전자상거래
+            </h1>
+        </div>
+    </div>
 )

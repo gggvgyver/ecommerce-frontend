716e5ef090bf279521355735873a82ff83207ade
gggvgyver <gggvgyver@gmail.com>
2021-01-31 12:21:04 +0900

    Link보완을 위해 a태크 사용

---
+++ utils/urls.js
@@ -0,0 +1,19 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
+
+/**
+ * 이미지를 URL로
+ * 로컬과 배포된 strapis를 위한 작업
+ * @param {any} image
+ */
+
+ export const fromImageToUrl = (image) => {
+     if (!image) {
+         return "/vercel.svg"
+     }
+
+     if (image.url.indexOf("/") === 0) {
+         return `${API_URL}${image.url}`
+     }
+
+     return image.url
+ }
